20cb32e5ad2600d6b6d9249fbe6d64eb6f8687c7
Luke Breen <l.breen1@outlook.com>
2017-06-18 18:24:47 +0100

    Added ajax to dashboard so that the attempt results for each exercise dynamically update without a new html request

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require highcharts/highcharts-more
-//= require chartkick
 //= require_tree .
 //= require serviceworker-companion
