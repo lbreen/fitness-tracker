2c5b49aac13fb230fb8357462e6a77438d793e7a
Luke Breen <l.breen1@outlook.com>
2017-06-22 17:32:41 +0100

    Inline form validation

--- app/models/attempt.rb
+++ app/models/attempt.rb
@@ -2,6 +2,7 @@ class Attempt < ApplicationRecord
   belongs_to :exercise
   validates :time_taken, presence: true # greater than 0
   validates :date, presence: true
+  before_create :cannot_be_in_the_future
 
   def user
     self.exercise.user
@@ -9,5 +10,9 @@ class Attempt < ApplicationRecord
 
   def time_taken_in_minutes
     Time.at(self.time_taken).utc.strftime("%M mins %S secs")
+  end
+
+  def cannot_be_in_the_future
+    self.date < Date.today
   end
 end
