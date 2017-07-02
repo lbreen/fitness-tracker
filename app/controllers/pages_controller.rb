5da64906898382f9b79643428a517ec60b85a37f
Luke Breen <l.breen1@outlook.com>
2017-07-02 20:14:34 +0100

    Added metricsgraph to exercise#show and pages#dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,6 +9,7 @@ class PagesController < ApplicationController
     @exercise = @exercises.first
     @attempts = @exercise.attempts.sort { |x, y| x.date <=> y.date }
     @attempt = Attempt.new # Create a new attempt from the new_attempt_modal
-    # @data = exercise.attempts.map { |attempt| [attempt.date, Time.at(attempt.time_taken).strftime("%H:%M:%S")]}
+    @data = @exercise.attempts.map { |attempt| {'date' => attempt.date, 'time_taken' => Time.at(attempt.time_taken).strftime("%H:%M:%S")}}
+    binding.pry
   end
 end
